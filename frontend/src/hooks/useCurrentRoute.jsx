e50ebe62bd4ba98a42700d589d366a782d43e76f
Jordan Kowal <kowaljordan@gmail.com>
2023-06-29 16:28:02 +0200

    Added useMemo

--- frontend/src/hooks/useCurrentRoute.jsx
+++ frontend/src/hooks/useCurrentRoute.jsx
@@ -1,9 +1,13 @@
+import { useMemo } from 'react';
 import { matchRoutes, useLocation } from 'react-router-dom';
 import { routeConfig } from '@/routes';
 
 const useCurrentRoute = () => {
   const location = useLocation();
-  const routes = matchRoutes(Object.values(routeConfig), location.pathname);
+  const routes = useMemo(
+    () => matchRoutes(routeConfig, location.pathname),
+    [location.pathname]
+  );
   return routes ? routes[0].route : undefined;
 };
 
