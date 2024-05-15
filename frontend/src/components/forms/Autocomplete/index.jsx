01172b4cbdef7f3ead2c4ab674b1888f1427bfd2
Jordan Kowal <kowaljordan@gmail.com>
2024-05-15 18:20:25 +0200

    fix: autocomplete style

--- frontend/src/components/forms/Autocomplete/index.jsx
+++ frontend/src/components/forms/Autocomplete/index.jsx
@@ -2,7 +2,6 @@ import React, { memo, useCallback, useMemo, useState } from 'react';
 import { Select, Typography } from 'antd';
 import PropTypes from 'prop-types';
 import { useDebounce } from 'react-use';
-import styles from './styles.module.less';
 
 const { Text } = Typography;
 
@@ -102,7 +101,6 @@ const Autocomplete = ({
 
   return (
     <Select
-      className={styles.autocomplete}
       onChange={onUpdateValue}
       value={value}
       mode={mode}
