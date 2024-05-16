e2bebe43cfa86373c61169de3ea13e8d05ec46d8
Jordan Kowal <kowaljordan@gmail.com>
2024-05-16 17:19:10 +0200

    chore: split dockerfile into 2, for dev and prod, and removed Autocomplete component

--- frontend/src/components/forms/index.jsx
+++ frontend/src/components/forms/index.jsx
@@ -1,4 +1,3 @@
-export { default as Autocomplete } from './Autocomplete';
 export { default as InputNumber } from './InputNumber';
 export { default as NonFieldErrors } from './NonFieldErrors';
 export { default as Switch } from './Switch';
