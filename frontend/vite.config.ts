493c81210441ef3e3726e94f67557892801d08d7
Jordan Kowal <kowaljordan@gmail.com>
2025-01-25 00:11:30 +0100

    chore: added react scan conditionnally on dev only

---
+++ frontend/vite.config.ts
@@ -0,0 +1,52 @@
+import react from "@vitejs/plugin-react";
+import million from "million/compiler";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+const reactScanPlugin = {
+  name: "inject-react-scan",
+  transformIndexHtml(html: string) {
+    return html.replace(
+      "</head>",
+      `<script src="https://unpkg.com/react-scan/dist/auto.global.js"></script></head>`,
+    );
+  },
+};
+
+defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const plugins = [million.vite({ auto: true }), react()];
+  if (isDev) plugins.push(reactScanPlugin);
+
+  return {
+    plugins: plugins,
+    server: {
+      port: 3000,
+      cors: true,
+      proxy: {
+        "^/(api)|(media)|(static)/": {
+          target: "http://api:8000",
+          changeOrigin: true,
+        },
+      },
+    },
+    esbuild: {
+      loader: "tsx",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+          ".ts": "tsx",
+        },
+      },
+    },
+    resolve: {
+      alias: { "@": resolve(__dirname, "./src") },
+    },
+    build: {
+      assetsDir: "static",
+      chunkSizeWarningLimit: 1024,
+    },
+  };
+});
