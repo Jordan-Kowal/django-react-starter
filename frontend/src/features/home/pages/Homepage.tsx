564799693ba59ed8217bc6e8245984a38a9b610e
Jordan Kowal <kowaljordan@gmail.com>
2025-03-15 01:40:15 +0100

    chore: Homepage basics

--- frontend/src/features/home/pages/Homepage.tsx
+++ frontend/src/features/home/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import { HeroLayout } from "@/components";
+import { HeroLayout, Logo } from "@/components";
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -7,17 +7,22 @@ const Homepage: React.FC = memo(() => {
 
   return (
     <HeroLayout dataTestId="homepage">
-      <h1 className="text-5xl font-bold">{t("Django React Starter")}</h1>
-      <p className="py-6">
-        {t("Browser-based multiplayer collaborative puzzle games")}
-      </p>
-      <div className="flex gap-4 justify-center">
-        <button type="button" className="btn btn-primary w-50">
-          {t("Create room")}
-        </button>
-        <button type="button" className="btn btn-secondary w-50">
-          {t("Join room")}
-        </button>
+      <div className="text-center">
+        <div className="max-w-50 mx-auto mb-6">
+          <Logo />
+        </div>
+        <h1 className="text-5xl font-bold">{t("Django React Starter")}</h1>
+        <p className="py-6">
+          {t("An easy way to start a Django + React project")}
+        </p>
+        <div className="flex gap-4 justify-center">
+          <button type="button" className="btn btn-primary w-50">
+            {t("Go to profile")}
+          </button>
+          <button type="button" className="btn btn-secondary w-50">
+            {t("Go to settings")}
+          </button>
+        </div>
       </div>
     </HeroLayout>
   );
