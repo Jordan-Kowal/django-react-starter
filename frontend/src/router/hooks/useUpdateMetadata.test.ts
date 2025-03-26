dfbb5bcab8d97d63d32033f6223074542b1c26f7
Jordan Kowal <kowaljordan@gmail.com>
2025-03-27 00:15:09 +0100

    test: router hooks

--- frontend/src/router/hooks/useUpdateMetadata.test.ts
+++ frontend/src/router/hooks/useUpdateMetadata.test.ts
@@ -1,16 +1,6 @@
-import { useLocale } from "@/hooks";
 import { renderHook } from "@testing-library/react";
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { useUpdateMetadata } from "./useUpdateMetadata";
-
-vi.mock("@/hooks", () => ({
-  useCurrentRoute: vi.fn(),
-  useLocale: vi.fn(() => ({
-    currentLocale: "en",
-    setLocale: vi.fn(),
-    setLocaleFromStorage: vi.fn(),
-  })),
-}));
 
 describe("useUpdateMetadata", () => {
   beforeEach(() => {
@@ -20,16 +10,13 @@ describe("useUpdateMetadata", () => {
 
   it("should update document title based on route staticData", () => {
     const mockTitle = "Django React Starter";
-    vi.mocked(useLocale).mockReturnValue({ currentLocale: "en" } as any);
     renderHook(() => useUpdateMetadata());
     expect(document.title).toBe(mockTitle);
   });
 
-  it("should not update document title when currentRoute is not available", () => {
-    const initialTitle = "Initial Title";
-    document.title = initialTitle;
-    vi.mocked(useLocale).mockReturnValue({ currentLocale: "en" } as any);
+  it("should fallback to default title if route is not handlded", () => {
+    document.title = "Initial Title";
     renderHook(() => useUpdateMetadata());
-    expect(document.title).toBe(initialTitle);
+    expect(document.title).toBe("Django React Starter");
   });
 });
